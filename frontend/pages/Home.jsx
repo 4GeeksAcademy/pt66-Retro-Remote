ff166d82e79768392d3591f18cd83d2ad91c601a
sindhura14 <sindhu.kilaru14@gmail.com>
2024-07-23 19:24:35 +0000

    initial code for displaying movie  details

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,15 +1,38 @@
+import { func } from "prop-types";
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
 
   const {store, dispatch} =useGlobalReducer()
+  const [movieId,setMovieId] = useState();
+
+  async function handleGetData(){
+	const options = {
+		method: 'GET',
+		headers: {
+		  accept: 'application/json',
+		}
+	  };
+	  const resp = await fetch('https://api.themoviedb.org/3/movie/1022789?language=en-US&api_key=f0d14b30a61125698e4990acdb103e21')
+	  const data = await resp.json();
+	  console.log(data);
+	  console.log(data.id)
+	  setMovieId(data.id)
+  }
+
+
 
 	return (
 		<div className="text-center mt-5">
 			<h1>Hello Rigo!!</h1>
 			<p>
 				<img src={rigoImageUrl} />
+				<button type="button" className="btn btn-primary" onClick={()=>handleGetData()}>Get Data</button>
+				<Link to={`/movie/${movieId}`}><button className="btn btn-dark">More Details</button></Link>
+
 			</p>
 		</div>
 	);
--- frontend/pages/Layout.jsx
+++ frontend/pages/Layout.jsx
@@ -6,10 +6,8 @@ import { Footer } from "../components/Footer"
 // Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
     return (
-        <ScrollToTop>
-            <Navbar />
+
                 <Outlet />
-            <Footer />
-        </ScrollToTop>
+
     )
 }
